1b1cbc03de04a9d5ad13ab24f637c0ac01d56dae
HasnenSheikh <mhasnen903@gmail.com>
2026-07-29 02:05:56 +0530

    Milestone-2 Base-UI-Templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from models import db
 
 app = Flask(__name__)
@@ -7,8 +7,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'your_super_secret_key' 
 
-
 db.init_app(app)
+
+@app.route('/')
+def home():
+    # Redirecting home to login
+    return render_template('login.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- create_db.py
+++ create_db.py
@@ -1,20 +1,18 @@
 from app import app
 from models import db, User
 
-# We use app_context() because SQLAlchemy needs to know which Flask app it is working with
 with app.app_context():
-    # 1. Create all tables defined in models.py
+
     db.create_all()
     
-    # 2. Check if an Admin already exists
     admin = User.query.filter_by(role='Admin').first()
     
     if not admin:
-        # 3. Create the default Admin user
+
         new_admin = User(
             full_name='Admin Superuser',
-            email='admin@trek.com',
-            password='adminpassword', # We will hash this securely in Phase 4
+            email='admin@gmail.com',
+            password='@dmin123',
             role='Admin',
             status='Approved'
         )
